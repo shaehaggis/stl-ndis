5c8d7dc87f15f32fdb6b7f22900f2b5076da089c
shaehaggis <shae.haggis@gmail.com>
2023-12-04 13:23:09 +1030

    fixed logout bug that redirects back to home. display modal on view-jobs page

--- app/src/main/java/com/example/stl_ndis/ui/viewmodels/HomeViewModel.kt
+++ app/src/main/java/com/example/stl_ndis/ui/viewmodels/HomeViewModel.kt
@@ -5,7 +5,7 @@ import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.edit
+//import androidx.datastore.preferences.core.edit
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.stl_ndis.data.datastore.UserSettingsKeys
